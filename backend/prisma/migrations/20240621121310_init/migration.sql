32ee09376266b6a70b9c3fd6ca91810a2e88dbcf
amanDhiran <amandhiran201@gmail.com>
2024-06-21 23:34:43 +0530

    set up expres, passportjs and prisma

---
+++ backend/prisma/migrations/20240621121310_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "googleId" TEXT NOT NULL,
+    "githubId" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,43 @@
-import express from 'express'
-import { WebSocketServer } from 'ws';
+import express from "express";
+import { WebSocketServer } from "ws";
 // import { Chess } from 'chess.js'
-import http from 'http'
-import { GameManager } from './GameManager.js';
-import { log } from 'console';
+import http from "http";
+import { GameManager } from "./GameManager.js";
+import session from 'express-session';
+import { initPassport } from "./passport.js";
 
 const app = express();
 const server = http.createServer(app);
+
 const wss = new WebSocketServer({ server });
 
 const gameManager = new GameManager();
 
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET || "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
+  })
+);
+
+initPassport();
+app.use(passport.initialize());
+app.use(passport.authenticate("session"));
 
 wss.on("connection", (socket) => {
-    console.log("user connected");
-    gameManager.addUser(socket)
+  console.log("user connected");
+  gameManager.addUser(socket);
 
-    socket.on("close", () => {
-        console.log('user disconnected');
-        gameManager.removeUser(socket)
-    })    
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    gameManager.removeUser(socket);
+  });
 });
 
+app.use('/api/v1/user', userRouter)
+
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
